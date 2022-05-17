44f9274249ac897fc7f4e853e75e6c2d2d491627
joshuauzor <joshuauzor10@gmail.com>
2022-05-17 07:24:04 +0100

    orientation and device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:newsnow/app/view/app.dart';
 import 'package:newsnow/bootstrap.dart';
@@ -7,8 +9,14 @@ import 'package:newsnow/injections.dart';
 Future<void> main() async {
   await dotenv.load();
   configureDependencies();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]);
   await bootstrap(
     () => DevicePreview(
+      // ignore: avoid_redundant_argument_values
+      enabled: !kReleaseMode,
       builder: (context) => const App(),
     ),
   );
